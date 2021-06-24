b520cfd4b7ad25ea268ff7826adf437ab12e499b
BRAJESH997 <40537685+BRAJESH997@users.noreply.github.com>
2021-06-25 00:54:16 +0530

    Desktop Version Completed

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function imgSlider(event){
+	document.querySelector('.pepsi').src=event;
+}
+
+
+function bgChanger(event){
+	const sec=document.querySelector('.sec');
+	sec.style.background = event;
+}
